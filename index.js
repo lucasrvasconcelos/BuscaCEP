86d52b9e72ed3b6c88acf22b0b95cf4b66aae625
lucasrvasconcelos <lucasvasconcelos900@gmail.com>
2023-04-27 16:04:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const app = express()
 const handlebars = require("express-handlebars");
-const port = process.env.PORT || 3000
+const port = 3000
+
 app.use(express.static("public"))
 app.use(express.static("views"))
 app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
