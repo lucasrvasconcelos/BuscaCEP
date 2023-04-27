89db455edbe0233ad8ae6d56b9c7c2c51595e999
lucasrvasconcelos <lucasvasconcelos900@gmail.com>
2023-04-27 14:56:02 -0300

    Vercel

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const handlebars = require("express-handlebars");
-
+const port = process.env.PORT || 3000
 app.use(express.static("public"))
 
 app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
@@ -12,6 +12,6 @@ app.get('/', (req, res) =>{
     res.render("buscacep")
 })
 
-app.listen(3000, () => {
-    console.log("Conectado a porta 3000")
+app.listen(port, () => {
+    console.log("Conectado a porta " + port)
 })
