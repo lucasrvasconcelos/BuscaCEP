72fb16faa0975f40e99cdf6a60561b1481da716b
lucasrvasconcelos <lucasvasconcelos900@gmail.com>
2023-04-27 16:54:32 -0300

    commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path")
 const handlebars = require("express-handlebars");
 const port = process.env.PORT || 3000 
 
-app.use(express.static("public"))
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
